1b33a3bc949d3a0f182e85b758934ea25798d128
mdvickst <mdvickst@users.noreply.github.com>
2025-12-10 17:30:20 -0500

    replace deprecated interruptByDtmf
    Replaced interruptByDtmf="false" with interruptible="speech"

--- src/controllers/callController.ts
+++ src/controllers/callController.ts
@@ -16,7 +16,7 @@ export async function handleIncomingCall(
   // Refer the ConversationRelay docs for a complete list of attributes - https://www.twilio.com/docs/voice/twiml/connect/conversationrelay#conversationrelay-attributes
   return `<Response>
               <Connect action="https://${config.ngrok.domain}/api/action">
-                    <ConversationRelay url="wss://${config.ngrok.domain}" dtmfDetection="true" interruptByDtmf="false" welcomeGreeting="${config.twilio.welcomeGreeting}">
+                    <ConversationRelay url="wss://${config.ngrok.domain}" dtmfDetection="true" interruptible="speech" welcomeGreeting="${config.twilio.welcomeGreeting}">
                       <Language code="es-US" ttsProvider="ElevenLabs" voice="h415g7h7bSwQrn1qw4ar" />
                       <Language code="en-US" ttsProvider="ElevenLabs" voice="g6xIsTj2HwM6VR4iXFCw" />
                     </ConversationRelay>
